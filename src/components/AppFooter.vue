64ac7d4f83b5734defc65b46a3a6150df4121a31
“ManonCacciatore” <cacciatoremanon8@gmail.com>
2025-11-17 15:14:01 +0100

    Architecture des components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+      <footer class="my-6 text-slate-400 text-sm text-center">
+    <p>Simple Todo Template — Tailwind CSS · EAFC 2025</p>
+  </footer>
+</template>
+<style scoped></style>
